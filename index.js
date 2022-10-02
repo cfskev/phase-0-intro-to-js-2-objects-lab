bec92d4239a5d567221ef1fbd00fda3a4cd24551
Kevin <kevin@crownfundingsource.com>
2022-10-02 18:13:37 -0400

    commit tree 73733aea43cd858424fcdb2a5fcd46b480e509a2

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Kevin",
+    streetAddress : "8238 St James Ave",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newObj = {...object};
+
+    newObj[key] = value;
+
+    return newObj;
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+    employee[key]=value;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj ={...employee};
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
+
+
